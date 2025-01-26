f868058cd253214143633719739d074a07dbae95
mbhuet <mbhuet@gmail.com>
2025-01-26 01:36:19 -0600

    intro sequence

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,7 +1,21 @@
+using DG.Tweening;
+using NUnit.Framework.Constraints;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    private enum State
+    {
+        COMIC_INTRO, //comic panels
+        COWGIRL_INTRO, //ui appears
+        GAME,
+        OUTRO,
+    }
+
+    private State currentState;
 
     public static GameManager Instance;
 
@@ -11,6 +25,15 @@ public class GameManager : MonoBehaviour
 
     public BobaTeaManager TeaManager => _teaManager;
 
+    public Transform _uiPanelRoot;
+    public Image[] _comicPanels;
+    public float _comicPanelDelayTime = 2f;
+    public float _comicPanelFadeTime = .2f;
+    public SpriteRenderer _cowgirlSprite;
+    public DelilahStateMachine _cowgirlStateMachine;
+
+    private bool _bobaTeaServeNeeded = true;
+    private Coroutine _bobaServeRoutine;
     private void Awake()
     {
         if (Instance == null)
@@ -18,7 +41,26 @@ public class GameManager : MonoBehaviour
         else
             Destroy(this);
 
-        DontDestroyOnLoad(Instance);
+        //DontDestroyOnLoad(Instance);
+
+
+        ResetUI();
+    }
+
+
+    private void ResetUI()
+    {
+        _uiPanelRoot.transform.localPosition = Vector3.left * 500;
+        for (int comicIndex = 0; comicIndex < _comicPanels.Length; comicIndex++)
+        {
+            _comicPanels[comicIndex].gameObject.SetActive(false);
+        }
+        _cowgirlSprite.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        SetState(State.COMIC_INTRO);
     }
 
     // Update is called once per frame
@@ -33,13 +75,38 @@ public class GameManager : MonoBehaviour
             _teaManager.BobaSucked?.Invoke(bobaPearl);
         }
 #endif
+        UpdateState();
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    private void UpdateState()
+    {
+        switch (currentState)
+        {
+            case State.COMIC_INTRO:
+                break;
+            case State.GAME:
+                if (_bobaTeaServeNeeded)
+                {
+                    _bobaServeRoutine = StartCoroutine(BobaServeRoutine());
+                }
+                break;
+            case State.OUTRO:
+                break;
+            case State.COWGIRL_INTRO:
+                break;
+        }
     }
 
     public void AssignBobaManager(BobaTeaManager manager)
     {
         if (manager == null)
             return;
-        
+
         _teaManager = manager;
         _teaManager.BobaSucked += OnBobaSucked;
     }
@@ -72,5 +139,99 @@ public class GameManager : MonoBehaviour
         }
 
         Debug.Log($"Score: {_score}", this);
+    }
+
+    private void SetState(State newState)
+    {
+        switch (currentState)
+        {
+            case State.COMIC_INTRO:
+                break;
+            case State.GAME:
+                break;
+            case State.OUTRO:
+                break;
+            case State.COWGIRL_INTRO:
+                break;
+        }
+
+        switch (newState)
+        {
+            case State.COMIC_INTRO:
+                StartCoroutine(IntroRoutine());
+                break;
+            case State.GAME:
+                break;
+            case State.OUTRO:
+                break;
+            case State.COWGIRL_INTRO:
+                break;
+        }
+        currentState = newState;
+    }
+
+    private IEnumerator IntroRoutine()
+    {
+        yield return DelayOrClickToAdvance();
+
+        //fade in comic panels one at a time
+        for (int comicIndex = 0; comicIndex < _comicPanels.Length; comicIndex++)
+        {
+            Image comic = _comicPanels[comicIndex];
+            comic.gameObject.SetActive(true);
+            comic.color = Color.clear;
+            comic.DOColor(Color.white, _comicPanelFadeTime);
+            yield return DelayOrClickToAdvance();
+        }
+
+        //fade in cowgirl
+        _cowgirlSprite.gameObject.SetActive(true);
+        _cowgirlStateMachine.SetDelilahState(DelilahStateMachine.DelilahState.Default);
+        _cowgirlSprite.color = Color.clear;
+        _cowgirlSprite.DOColor(Color.white, _comicPanelFadeTime);
+
+        yield return DelayOrClickToAdvance();
+
+
+        for (int comicIndex = 0; comicIndex < _comicPanels.Length; comicIndex++)
+        {
+            Image comic = _comicPanels[comicIndex];
+            DOTween.Kill(comic);
+            comic.DOColor(Color.clear, _comicPanelFadeTime);
+        }
+
+        _uiPanelRoot.DOMoveX(0, 1).SetEase(Ease.InOutBack);
+
+        yield return DelayOrClickToAdvance();
+
+        _bobaTeaServeNeeded = true;
+
+
+
+        SetState(State.GAME);
+        yield return null;
+    }
+
+    private IEnumerator BobaServeRoutine()
+    {
+        _bobaTeaServeNeeded = false;
+        _cowgirlStateMachine.SetDelilahState(DelilahStateMachine.DelilahState.Celebratory);
+        yield return null;
+    }
+
+    private IEnumerator DelayOrClickToAdvance()
+    {
+        float t = 0;
+        while (t < _comicPanelDelayTime && !ClickToAdvance())
+        {
+            t += Time.deltaTime;
+            yield return null;
+        }
+        yield return null;
+    }
+
+    private bool ClickToAdvance()
+    {
+        return Input.GetMouseButtonDown(0);
     }
 }
